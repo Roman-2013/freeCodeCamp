07c77c42a3a4541f022333d1002311ee79c88941
Roman-2013 <roma.pushkarev.1995@mail.ru>
2023-04-26 16:41:46 +0300

    Update freeCodeCampJS.js

--- freeCodeCampJS.js
+++ freeCodeCampJS.js
@@ -136,7 +136,7 @@ function golfScore(par, strokes) {
   // Only change code below this line
 if(strokes==1){
   return names[0];
-  
+
 }else if(strokes<=par-2){
   return names[1];
 }else if(strokes==par-1){
@@ -156,3 +156,122 @@ if(strokes==1){
 }
 
 golfScore(5, 4);
+
+// Выбор из множества вариантов с операторами Switch
+function caseInSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+switch(val){
+  case 1:
+  answer="alpha";
+  break;
+
+  case 2:
+  answer="beta";
+  break;
+
+  case 3:
+  answer="gamma";
+  break;
+
+  case 4:
+  answer="delta";
+  break
+}
+
+
+  // Only change code above this line
+  return answer;
+}
+
+caseInSwitch(1);
+
+// Добавление параметра по умолчанию в операторы Switch
+function switchOfStuff(val) {
+  let answer = "";
+  // Only change code below this line
+switch(val){
+  case 'a':
+  answer="apple";
+  break
+  
+  case 'b':
+  answer="bird";
+  break;
+
+  case 'c':
+  answer="cat";
+  break;
+
+  default:
+  answer="stuff";
+  break;
+}
+
+
+  // Only change code above this line
+  return answer;
+}
+
+switchOfStuff(1);
+
+//Несколько идентичных опций в операторах switch
+function sequentialSizes(val) {
+  let answer = "";
+  // Only change code below this line
+switch(val){
+  case 1:
+  case 2:
+  case 3:
+  answer="Low";
+  break;
+
+  case 4:
+  case 5:
+  case 6:
+  answer="Mid";
+  break;
+
+  case 7:
+  case 8:
+  case 9:
+  answer="High";
+  break;
+}
+
+
+  // Only change code above this line
+  return answer;
+}
+
+sequentialSizes(1);
+
+// Замена цепочек If Else на Switch
+
+function chainToSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+
+  
+  switch(val){
+    case "bob":
+    answer = "Marley";
+    break;
+    case 42:
+     answer = "The Answer";
+     break;
+     case 1:
+     answer = "There is no #1";
+     break;
+     case 99:
+     answer = "Missed me by this much!";
+     break;
+     case 7:
+      answer = "Ate Nine";
+      break;
+  }
+  // Only change code above this line
+  return answer;
+}
+
+chainToSwitch(7);
