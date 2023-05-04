1514ca73810cdd2fd9db141d275fb165f3cebdd5
Roman-2013 <roma.pushkarev.1995@mail.ru>
2023-05-04 14:11:00 +0300

    Update ES6.js

--- ES6.js
+++ ES6.js
@@ -57,3 +57,25 @@ const HIGH_TEMPERATURES = {
 };
 
 const{today: highToday, tomorrow: highTomorrow}=HIGH_TEMPERATURES
+
+
+//Create Strings using Template Literals
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["no-extra-semi", "no-dup-keys"]
+};
+function makeList(arr) {
+  // Only change code below this line
+  const failureItems = [];
+  for(let i =0 ; i<arr.length ; i++){
+    failureItems.push(`<li class="text-warning">${arr[i]}</li>`)
+  };
+  // Only change code above this line
+
+  return failureItems;
+}
+
+const failuresList = makeList(result.failure);
+
+console.log(failuresList);
